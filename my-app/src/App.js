fad8d41a27d36ee5e2a63066059c7fb431a2b198
Yujing9 <244242977@qq.com>
2023-03-21 00:31:13 +0000

    20/3 commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,15 +5,13 @@ import {ImageLinkForm} from './Components/ImageLinkForm/ImageLinkForm';
 import {Rank} from './Components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
 import { useState } from 'react';
+import { FaceRecognition } from './Components/FaceRecognition/FaceRecognition';
 
+// Connect to the charifai api
 const detectImage = (ImageURL)=>{
-  // Your PAT (Personal Access Token) can be found in the portal under Authentification
 const PAT = 'cda738f0768c47089890fca808d69ee9';
-// Specify the correct user_id/app_id pairings
-// Since you're making inferences outside your app's scope
 const USER_ID = 'cfi6zxrvdqao';       
 const APP_ID = 'test';
-// Change these to whatever model and image URL you want to use
 const MODEL_ID = 'face-detection';
 const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
 const IMAGE_URL = ImageURL;
@@ -42,11 +40,7 @@ const requestOptions = {
     },
     body: raw
 };
-
-// NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-// https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-// this will default to the latest version_id
-
+// todo:NEED TO FIND WHAT OUTPUT DO WE WANT
 fetch(`https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`, requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
@@ -54,8 +48,10 @@ fetch(`https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_I
   
 }
 
+// https://samples.clarifai.com/face-det.jpg
+
 function App() {
-  const [url,setUrl] = useState('https://samples.clarifai.com/face-det.jpg');
+  const [url,setUrl] = useState('');
   const onInputChange = (event)=>{
     const imageUrl = event.target.value
     setUrl(imageUrl)
@@ -73,6 +69,7 @@ function App() {
       <Logo />
       <Rank />
       <ImageLinkForm onInputChange = {onInputChange} onButtonSubmit ={onButtonSubmit}/>
+      <FaceRecognition imgUrl={url}/>
       <ParticlesBg type="polygon" bg={true} />
     </div>
     
---
+++ my-app/src/Components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,9 @@
+export function FaceRecognition(props){
+    return (
+        <div className = "center">
+        <div className = "absoluate mt2">
+            <img src={props.imgUrl} alt="test" width="500px" height = "auto"></img>
+            </div>
+        </div>
+    );
+}
